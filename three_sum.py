a39b8b1b10c18559646ab0f67b82fa7c82f91c6b
Dragon-L <530593201@qq.com>
2019-04-21 23:03:05 +0800

    three sum

---
+++ three_sum.py
@@ -0,0 +1,55 @@
+# 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+#
+# 注意：答案中不可以包含重复的三元组。
+#
+# 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+#
+# 满足要求的三元组集合为：
+# [
+#   [-1, 0, 1],
+#   [-1, -1, 2]
+# ]
+# cost 748ms and beat 91.56%
+#
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        self.nums_dict = {}
+        result = []
+
+        for num in nums:
+            if num in self.nums_dict:
+                self.nums_dict[num] += 1
+            else:
+                self.nums_dict[num] = 1
+
+        nums = sorted(set(nums))
+
+        for i in range(len(nums)):
+            sum_list = self._two_sum(nums, -nums[i], i)
+            if sum_list:
+                result.extend(sum_list)
+        return result
+
+    def _two_sum(self, nums: List[int], target: int, start_index: int) -> List[List[int]]:
+        result = []
+        for i in range(start_index, len(nums)):
+            if target - nums[i] in self.nums_dict:
+                is_three_different_nums = -target != nums[i] and nums[i] < target - nums[i]
+                is_first_two_common_nums = is_three_different_nums or (-target == nums[i] and self.nums_dict[-target] >= 2 and nums[i] < target - nums[i])
+                is_last_two_common_nums = is_first_two_common_nums or (-target != nums[i] and nums[i] == target - nums[i] and self.nums_dict[nums[i]] >= 2)
+                is_three_common_nums = is_last_two_common_nums or (-target == nums[i] and nums[i] == target - nums[i] and self.nums_dict[-target] >= 3)
+
+                if is_three_common_nums:
+                    result.append([-target, nums[i], target - nums[i]])
+        return result
+
+solution = Solution()
+
+# assert solution.threeSum([0, 0, 0, 0]) == [[0, 0, 0]]
+# assert solution.threeSum([0, 0]) == []
+# assert solution.threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, 0, 1], [-1, -1, 2]]
+# assert solution.threeSum([]) == []
+assert solution.threeSum([1, 2, -2, -1]) == []
