b553d007e38886b3554526a5d475425727ce146f
Dragon-L <530593201@qq.com>
2019-04-30 00:42:43 +0800

    merge k list v2

--- merge_k_lists.py
+++ merge_k_lists.py
@@ -11,7 +11,7 @@
 # 输出: 1->1->2->3->4->4->5->6
 #
 # v1： over time
-#
+# v2: 100ms and beat 80.74%
 #
 from math import inf
 from typing import List, Tuple
@@ -42,22 +42,39 @@ class Solution:
     def mergeKLists_v2(self, lists: List[ListNode]) -> ListNode:
         merged_list = ListNode(None)
         current_node = merged_list
-        while True:
-            self._sort_lists(lists)
+        self._merge_k_lists(lists, current_node)
+        return merged_list.next
 
-    def _sort_lists(self, lists: List[ListNode]) -> List[Tuple[int, int]]:
-        pass
+    def _merge_k_lists(self, lists: List[ListNode], current_node: ListNode) -> None:
+        lists.sort(key=lambda x: inf if x is None else x.val)
+        second_smallest_val = inf
+        for i in range(len(lists)):
+            if lists[i] is None or second_smallest_val < lists[i].val:
+                break
+
+            current_node.next = lists[i]
+            current_node = current_node.next
+            lists[i] = lists[i].next
+
+            if lists[i] is not None and lists[i].val < second_smallest_val:
+                second_smallest_val = lists[i].val
+        if second_smallest_val == inf:
+            return
+        else:
+            self._merge_k_lists(lists, current_node)
+
+
 
 
 def test():
     solution = Solution()
 
-    assert node_to_list(solution.mergeKLists_v1(
+    assert node_to_list(solution.mergeKLists_v2(
         [list_to_node([1, 4, 5]), list_to_node([1, 3, 4]), list_to_node([2, 6])])) \
            == [1, 1, 2, 3, 4, 4, 5, 6]
 
-    assert node_to_list(solution.mergeKLists_v1([None, None, None])) == []
-    assert node_to_list(solution.mergeKLists_v1([None, list_to_node([1]), None])) == [1]
-    assert node_to_list(solution.mergeKLists_v1([None, list_to_node([1, 2]), None])) == [1, 2]
+    assert node_to_list(solution.mergeKLists_v2([None, None, None])) == []
+    assert node_to_list(solution.mergeKLists_v2([None, list_to_node([1]), None])) == [1]
+    assert node_to_list(solution.mergeKLists_v2([None, list_to_node([1, 2]), None])) == [1, 2]
 
 test()
