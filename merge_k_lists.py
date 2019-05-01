ed09cd99f30e1146ba3cda615f69dfa25426f7c1
Dragon-L <530593201@qq.com>
2019-05-01 21:58:03 +0800

    merge k list

--- merge_k_lists.py
+++ merge_k_lists.py
@@ -12,7 +12,7 @@
 #
 # v1： over time
 # v2: 100ms and beat 80.74%
-#
+# v3: 分治：172ms and beat 37.86%, java version: 7ms and beat 89.31%
 from math import inf
 from typing import List, Tuple
 
@@ -63,18 +63,42 @@ class Solution:
         else:
             self._merge_k_lists(lists, current_node)
 
+    def mergeKLists_v3(self, lists: List[ListNode]) -> ListNode:
+        if len(lists) == 0:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        if len(lists) == 2:
+            return self._merge_two_lists(lists[0], lists[1])
 
+        mid = len(lists) // 2
+        return self._merge_two_lists(self.mergeKLists_v3(lists[:mid]), self.mergeKLists_v3(lists[mid:]))
+
+    def _merge_two_lists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+
+        if l1.val < l2.val:
+            merged_list = l1
+            merged_list.next = self._merge_two_lists(l1.next, l2)
+        else:
+            merged_list = l2
+            merged_list.next = self._merge_two_lists(l1, l2.next)
+
+        return merged_list
 
 
 def test():
     solution = Solution()
 
-    assert node_to_list(solution.mergeKLists_v2(
+    assert node_to_list(solution.mergeKLists_v3(
         [list_to_node([1, 4, 5]), list_to_node([1, 3, 4]), list_to_node([2, 6])])) \
            == [1, 1, 2, 3, 4, 4, 5, 6]
 
-    assert node_to_list(solution.mergeKLists_v2([None, None, None])) == []
-    assert node_to_list(solution.mergeKLists_v2([None, list_to_node([1]), None])) == [1]
-    assert node_to_list(solution.mergeKLists_v2([None, list_to_node([1, 2]), None])) == [1, 2]
+    assert node_to_list(solution.mergeKLists_v3([None, None, None])) == []
+    assert node_to_list(solution.mergeKLists_v3([None, list_to_node([1]), None])) == [1]
+    assert node_to_list(solution.mergeKLists_v3([None, list_to_node([1, 2]), None])) == [1, 2]
 
 test()
